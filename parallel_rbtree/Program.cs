13e4b721eea94a367aedb552ddc2337ef6255737
Munira <tursunova.munira@gmail.com>
2017-12-22 14:27:04 +0200

    Update Program.cs

--- parallel_rbtree/Program.cs
+++ parallel_rbtree/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,19 +9,16 @@ namespace parallel_rbtree
 {
     class Program
     {
-        static void Main(string[] args)
+        
+        public static void Main(String[] args) //throws InterruptedException
         {
-
-        }
-        public static void main(String[] args) //throws InterruptedException
-        {
-	
-		    long start = 0, end = 0, duration = 0;
+            
+            long start = 0, end = 0, duration = 0;
 		    // ---- customize ----
 		    int num_threads = 4;
 		    int insert_nodes_per_thread = 10;
 		    //Boolean visulize_locked_tree = false;
-            Boolean visulize_lock_free_tree = false;
+            Boolean visulize_lock_free_tree = true;
 		    // ---------
 		    //List<Thread> locked_threads = new List<Thread>();
 		    List<Thread> lock_free_threads = new List<Thread>();
@@ -30,19 +27,18 @@ namespace parallel_rbtree
 		    lock_free_threads.Clear();
 		    //LockedRBTree locked_tree = new LockedRBTree();
 		    LockFreeRBTree lock_free_tree = new LockFreeRBTree();
-
-		    Console.WriteLine(" -------- Insertion Test --------");
+            Random rand = new Random();
+            Console.WriteLine(" -------- Insertion Test --------");
             Console.WriteLine("Each thread inserts: " + insert_nodes_per_thread + " nodes");
             Console.WriteLine("Lock-Based Threads:");
 		    for (int i = 0; i < num_threads; i++) {
 			    int[] values = new int[insert_nodes_per_thread];
 			    for (int j = 0; j < insert_nodes_per_thread; j++) {
-				    Random rand = new Random();
-				    int  n = rand.Next(1000) + 1;
-				    values[j] = n;
+				   
+				    values[j] = rand.Next(1000);
 			    }
 			   // locked_threads.Add(new insertThread(locked_tree, values, lock));
-			    lock_free_threads.Add(new insertThread(lock_free_tree, values));
+			    lock_free_threads.Add(new insertThread(lock_free_tree, values).getInstance());
 		    }
 		    //start = System.currentTimeMillis();
 		    /*foreach(Thread thread in locked_threads){
@@ -80,6 +76,7 @@ namespace parallel_rbtree
 
             Console.WriteLine(" -------- Search Test --------");
             Console.WriteLine("Each thread search 10000 times");
+            
 		    /*locked_threads.clear();
 		    for (int i = 0; i < num_threads; i++) {
 			    locked_threads.add(new SearchThread(locked_tree,num_threads, insert_nodes_per_thread, lock));
@@ -97,19 +94,20 @@ namespace parallel_rbtree
 		    */
 
 		    lock_free_threads.Clear();
-		   /* for (int i = 0; i<num_threads; i++) {
-			    lock_free_threads.Add(new SearchThread(lock_free_tree, num_threads, insert_nodes_per_thread));
+		    for (int i = 0; i<num_threads; i++) {
+			    lock_free_threads.Add(new SearchThread(lock_free_tree, num_threads, insert_nodes_per_thread).getInstance());
 		    }
-		    start = System.currentTimeMillis();
+		   // start = System.currentTimeMillis();
 		    foreach(Thread thread in lock_free_threads) {
 			    thread.Start();
 		    }
 		    foreach(Thread thread in lock_free_threads){
 			    thread.Join();
 		    }
-		    end = System.currentTimeMillis();
-		    duration = end - start;
-		    Console.WriteLine("Lock-Free RBTree uses " +(double)duration + " ms");*/
-	    }
+            Console.ReadKey();
+            // end = System.currentTimeMillis();
+            //duration = end - start;
+            // Console.WriteLine("Lock-Free RBTree uses " +(double)duration + " ms");
+        }
     }
 }
