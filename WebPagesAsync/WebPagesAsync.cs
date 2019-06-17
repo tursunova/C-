e5505683e4828f27286435e1b95a2c3b74cd1669
Munira <tursunovamunira@gmail.com>
2019-06-17 12:40:48 +0500

    Update WebPagesAsync.cs

--- WebPagesAsync/WebPagesAsync.cs
+++ WebPagesAsync/WebPagesAsync.cs
@@ -1,20 +1,78 @@
 using System;
 using System.Collections.Generic;
+using System.Security.Policy;
+using System.Threading.Tasks;
 using System.Net;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace WebPagesAsync
 {
-    
-    internal class Program
+    public class Downloader
+    {
+        public int maxDepth;
+
+        public Downloader(int maxValueofDepth = 3)
+        {
+            maxDepth = maxValueofDepth;
+        }
+
+        public async Task GetPages(Uri uri, int depth = 0)
+        {
+            if (depth == maxDepth) return;
+            var str = await GetPageContent(uri.ToString());
+            
+            Console.WriteLine(uri.ToString() + " -- "+ str.Length + " | " + depth);
+
+            var links = new List<String>();
+            var tasks = new List<Task>();
+
+            links = GetLinks(uri);
+
+            foreach (var link in links)
+            {
+                //Console.WriteLine(link);
+                tasks.Add(GetPages(new Uri(link), depth + 1));
+            }
+            await Task.WhenAll(tasks.ToArray());
+            return;
+        }
+        private List<string> GetLinks(Uri uri)
+        {
+            var html = new WebClient().DownloadString(uri);
+            var reHref = new Regex(@"http(s)?://([\w-]+\.)+[\w-]+(/[\w- ./?%&=]*)?");
+            var links = new List<string>();
+            foreach (Match match in reHref.Matches(html))
+            {
+                //Console.WriteLine(match.Groups["url"].ToString());
+                links.Add(match.Value);
+            }
+            return links;
+
+        }
+
+        private async Task<String> GetPageContent(string url)
+        {
+            string str;
+            using (var client = new WebClient())
+            {
+                var bytes = await client.DownloadDataTaskAsync(url);
+                str = client.Encoding.GetString(bytes);
+            }
+            return str;
+        }
+
+    }
+    class Program
     {
         public static void Main(string[] args)
         {
+
             Downloader url = new Downloader(2);
 
-            Task task = url.GetPages(new Uri("https://youtube.com/"));
+            var task = url.GetPages(new Uri("https://Github.com/microsoft"));
             task.Wait();
+            
         }
     }
+
 }
